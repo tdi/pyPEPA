f1019e2fa4b4f2a7e9d9c39c7463d592326370c8
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2012-04-21 12:28:22 +0200

    Ziany, DFS

--- comp.py
+++ comp.py
@@ -6,6 +6,6 @@ class Component(Element):
         super(Component,self).__init__()
         self.name = name
         self.cclass = cclass
-    
+
     def __str__(self):
         return "name:" + self.name + ",class:" + self.cclass
--- model.py
+++ model.py
@@ -1,6 +1,18 @@
 
 
-class Model:
+class Model(object):
+    
+    def __init__(self, components, links, instances):
+        self.components = components
+        self.links = links
+        self.instances = instances
 
-  
+    def __init__(self):
+        self.components = []
+        self.links = []
+        self.instances = []
+
+    def connect_ports(self, port1, port2):
+        port1.other = port2
+        port2.other = port1
 
