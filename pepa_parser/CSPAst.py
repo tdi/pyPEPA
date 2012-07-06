41ba08ee589d361a29aac550c1575e708bb2c7a3
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2012-07-06 17:32:34 +0200

    Drzewo step 1

--- pepa_parser/CSPAst.py
+++ pepa_parser/CSPAst.py
@@ -4,8 +4,19 @@ class Model():
     rates = None
     costam = ""
 
+
+class SSNode():
+    succ = []
+    data = ""
+
+    def __init__(self, data):
+        self.data = data
+
+
 class Node():
-    left, right, data, asttype, actions = None, None, 0, None, None
+    left, right, data, asttype, actions, visited = None, None, 0, None, None, 0
+    activity, rate = "",""
+    resolved = ""
 
     def __init__(self,data,asttype):
         self.right = None
@@ -13,23 +24,60 @@ class Node():
         self.data = data
         self.asttype = asttype
         self.lhs = ""
-        actions = None
+        self.activity = ""
+        self.action = None
+        self.visited = 0
+        self.process = ""
+        self.resolved = ""
 
     def __str__(self):
         print(self.asttype)
 
 
+class BaseNode():
+    left,right = None, None
+    data = None
+    asttype = None
 
-def visit_tree(node):
-    print(node.data,end="")
-    if node.left is not None:
-        print("(", end="")
-        visit_tree(node.left)
-        print(" ",end="")
-    if node.right is not None:
-        visit_tree(node.right)
-        print(")", end="")
+    def __init__(self, data, asttype):
+        self.data = data
+        self.asttype = asttype
 
+    def __str__(self):
+        print(self.asttype)
+
+class ChoiceNode(BaseNode):
+    lhs, rhs = None, None
+    reolved = None
+
+    def __init__(self, data, asttype):
+        super().__init__(data, asttype)
+
+class PrefixNode(BaseNode):
+    action, resolved = None, None
+
+    def __init__(self, data, asttype):
+        super().__init__(data, asttype)
+
+class DefNode(BaseNode):
+    process, resolved = None, None
+
+    def __init__(self, data, asttype):
+        super().__init__(data, asttype)
+
+
+
+
+# def visit_tree(node):
+#     print(node.data,end="")
+#     if node.left is not None:
+#         print("(", end="")
+#         visit_tree(node.left)
+#         print(" ",end="")
+#     if node.right is not None:
+#         visit_tree(node.right)
+#         print(")", end="")
+#
 
 def tree_walker(node):
     visit_tree(node)
