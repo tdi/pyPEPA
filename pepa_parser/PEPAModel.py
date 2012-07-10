62110bf7803c642a27f2a18700b2006888b8ffde
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2012-07-10 14:56:37 +0200

    Visitor dla componentów

--- pepa_parser/PEPAModel.py
+++ pepa_parser/PEPAModel.py
@@ -9,16 +9,35 @@ from pyparsing import ParseException
 class SS():
     pass
 
-class PEPAModel():
+class ComponentStateVisitor():
 
+    def __init__(self, graph):
+        self.graph = graph
+        self.visited = []
+
+    def visit_print(self, node):
+        self.visited.append(node)
+        transitions = self.graph.ss[node].transitions
+        for tran in transitions:
+            print(node + " -> " + tran.to)
+            if tran.to not in self.visited:
+                self.visit_print(tran.to)
+
+
+class PEPAModel():
+    """
+        param modelfile is a path to a file with pepa model
+    """
     def __init__(self, modelfile):
         self.processes = {}
         self.systemeq = None
         self.rates = {}
+        self.seq_processes = {}
         self.tw = PEPATreeWalker()
         self.log = logging.getLogger(__name__)
         self._parse_read_model(modelfile)
         self._prepare_systemeq()
+        self._prepare_trees()
 
     def _prepare_systemeq(self):
         self.log.debug("Preparing systemeq")
@@ -32,7 +51,7 @@ class PEPAModel():
         with open(modelfile, "r") as f:
             modelfile = f.read()
         try:
-            parser = PEPAParser(True)
+            parser = PEPAParser(False)
             (self.processes, self.rates, self.systemeq) = parser.parse(modelfile)
         except ParseException as e:
             self.log.debug(e)
@@ -40,9 +59,14 @@ class PEPAModel():
             sys.exit(1)
 
     def _prepare_trees(self):
+        """ Here ss graphs of every process is derived from AST trees
+        """
         for node in self.processes.values():
-            self.tw.derive_processes(node)
-        self.tw.derive_whole_ss()
+            self.tw.derive_processes_ss(node)
+        visitor = ComponentStateVisitor(self.tw.graph)
+        for comp in self.seq_processes.keys():
+            self.log.debug("Deriving for component: " + comp)
+            visitor.visit_print(comp)
 
 
 
