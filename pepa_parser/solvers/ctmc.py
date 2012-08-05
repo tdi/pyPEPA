4d4088df1505cd6e645456739eefda06407da11e
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2012-08-05 10:00:42 +0200

    Solver and new test case (from Hillston's book)

---
+++ pepa_parser/solvers/ctmc.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import numpy
+import scipy
+
+def ctmc(Q):
+    b = numpy.zeros(len(Q), dtype=numpy.float64)
+    b[0] = 1
+    # normalization
+    Q[:,0] = 1
+    return numpy.linalg.solve(Q.transpose(),b)
+
+
+def vector_mult(v1, v2):
+    return float(numpy.vdot(numpy.array(v1), numpy.array(v2)))
+
+def create_matrix(res):
+    # create matrix lnum x lnum
+    size = len(res)
+    Q = numpy.zeros( (size, size), dtype=numpy.float64)
+    rowsum = 0.0
+    for key in sorted(res, key=lambda k: res[k][1]):
+        for tos in res[key][0]:
+            from_state = int( res[key][1] )
+            to_state = int( res[tos[1]][1] )
+            rate = float(tos[0])
+            Q[ from_state-1 , to_state-1 ] = rate
+            rowsum += rate
+        Q[ res[key][1]-1 , res[key][1]-1] = -rowsum
+        rowsum = 0.0
+    return Q
+
+
+
+if __name__ == "__main__":
+
+    x = ctmc(pepa_ss_parser("test_examples/model3.generator"))
+    for el in numpy.nditer(x):
+        if el < 0:
+            print("Error, probabilities are negative, the universe exploded just now")
+            break
+        else:
+            print(x)
