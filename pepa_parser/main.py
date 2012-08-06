25c46f122dc88ee8087497a77efba37575e1da5a
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2012-08-06 16:41:06 +0200

    pre branch

--- pepa_parser/main.py
+++ pepa_parser/main.py
@@ -5,6 +5,7 @@ import logging
 from pepa_model import PEPAModel
 import argparse
 
+
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
@@ -15,20 +16,9 @@ if __name__ == "__main__":
 
     pm = PEPAModel(args.file)
 
+
     if args.generate_dots:
         pm.generate_dots()
-#    pm = PEPAModel("test_files/simple.pepa")
-#    pm = PEPAModel("test_files/browser.pepa") # TEST ok
-    # pm = PEPAModel("test_files/comparison.pepa") # TEST ok
-    #pm = PEPAModel("test_files/REG_simple.pepa") # test OK
-#    pm = PEPAModel("test_files/REG-DB.pepa") # test OK
-    # pm = PEPAModel("test_files/car_scenario.pepa") # test OK
-#    pm = PEPAModel("test_files/lan4.pepa")
-    # pm = PEPAModel("test_files/test1_coop.pepa") # Test ok
-#    pm = PEPAModel("test_files/sri.pepa")
-    # pm = PEPAModel("test_files/resource.pepa")
-    #pm = PEPAModel("test_files/alternatingbit.pepa") # test ok
-    # pm = PEPAModel("test_files/bankscenario.pepa") #
 
 
 
