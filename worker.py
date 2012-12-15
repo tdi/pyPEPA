068b7339bad9ca5d800a7fffc84cb096d893f538
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2012-12-15 14:41:58 +0100

    Client i server

--- worker.py
+++ worker.py
@@ -2,11 +2,16 @@
 """Simple server that listens on port 6000 and echos back every input to the client.
 """
 from gevent.server import StreamServer
+import gevent
 import socket
 import pepa_prot
+import glob
+import os
+import sys
 from pepa_model import PEPAModel
+
 clients = set()
-models = ["resource.pepa"]
+models = list()
 
 def send_callback(address):
     ip = (address, 6001)
@@ -38,25 +43,56 @@ def process_cmd(data):
     ret = dict()
     if cmd == "list_models":
         ret["ret"] = models
+        ret["status"]  = 1
     elif cmd == "solve_ss":
         ret["ret"] = solve_ss(data["data"], data["ret"])
-
+        ret["status"]  = 1
+    elif cmd == "solve_th":
+        ret["ret"] = solve_th(data["data"], data["ret"])
+        ret["status"] = 1
     else:
         print("Command not found")
+        ret["status"]  = 0
         return None
     ret["rid"] = data["rid"]
     return ret
 
+
 def solve_ss(data, ret):
     print("Solving %s, returning %s" % (data, ret))
-    model = data
+    model = "models/%s" % data
     pm = PEPAModel({"file": model, "solver": "sparse"})
     pm.derive()
     pm.steady_state()
     ss = pm.get_steady_state_vector()
     return ss
 
+def solve_th(data, ret):
+    print("Solving %s, returning %s" % (data, ret))
+    model = "models/%s" % data
+    pm = PEPAModel( { "file" : model, "solver" : "sparse"})
+    pm.derive()
+    pm.steady_state()
+    th = pm.get_throughoutput()
+    return th
+
+def get_models():
+    global models
+    cwd = os.getcwd()
+    os.chdir("models")
+    models = glob.glob("*.pepa")
+    os.chdir(cwd)
+
+
+
+
 if __name__ == '__main__':
-    server = StreamServer(('0.0.0.0', 6000), process)
-    print ('Starting server on port 6000')
+    if os.path.isdir("models"):
+        get_models()
+    else:
+        print("No models directory")
+        exit(1)
+    port = int(sys.argv[1])
+    server = StreamServer(('0.0.0.0', port), process)
+    print ('Starting server on port %d' % port)
     server.serve_forever()
