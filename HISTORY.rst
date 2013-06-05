36e340940975f50b80b13979130bd4a893e01a39
Dariusz Dwornikowski <dariusz.dwornikowski@gmail.com>
2013-06-05 10:22:12 +0200

    History, licence and packaging

---
+++ HISTORY.rst
@@ -0,0 +1,13 @@
+.. :changelog:
+
+History
+-------
+
+0.4dev (05.06.2013)
++++++++++++++++++++
+
+ - Merge with Allan Clark's pull request providing better API
+ - Changed name from libpepa to pypepa where possible  
+ - First package version, for now only for dev branch 
+ 
+ 
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import pypepa
+
+try:
+        from setuptools import setup
+except ImportError:
+        from distutils.core import setup
+
+requires = ["matplotlib >= 1.2.0", "numpy>= 1.6.0", "scipy>=0.10", "colorama>=0.2.4"]
+
+setup (
+        name = "pypepa",
+        version = pypepa.__version__,
+        description = "pypepa is a PEPA library and a toolset for PEPA.",
+        long_description = open("README.md").read() + '\n\n' +
+                           open("HISTORY.rst").read(),
+        author = "Dariusz Dwornikowski", 
+        author_email = "dariusz.dwornikowski@cs.put.poznan.pl",
+        url = "https://github.com/tdi/pyPEPA",
+        packages = ["pypepa",
+                    "pypepa.parsing",
+                    "pypepa.derivation",
+                    "pypepa.experiments",
+                    "pypepa.solvers",
+            ],
+        package_data={'': ['LICENCE.txt']},
+        install_requires = requires,
+        license = open("LICENCE.txt").read(),
+        zip_safe = False,
+        entry_points = {
+            'console_scripts': ['pypepa=pypepa.py',]
+            },
+        classifiers = (
+            'Environment :: Console',
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 2.7',
+            'License :: OSI Approved :: Apache Software License',
+            'Intended Audience :: Science/Research'
+            ),
+        )
+
+
