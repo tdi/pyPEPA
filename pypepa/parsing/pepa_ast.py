d26563d20a778d02c34f293b693cd582d31da4b6
Allan Clark <a.d.clark@ed.ac.uk>
2013-07-25 10:55:48 +0100

    Tidied up the pepa_ast so that the names of the asttype are within the pepa_ast module. This means one cannot incorrectly create a ast node with the incorrect string for the asttype.

--- pypepa/parsing/pepa_ast.py
+++ pypepa/parsing/pepa_ast.py
@@ -17,43 +17,43 @@ class BaseNode(object):
 
 class ChoiceNode(BaseNode):
 
-    def __init__(self, data, asttype):
-        super(ChoiceNode, self).__init__(data, asttype)
+    def __init__(self, data):
+        super(ChoiceNode, self).__init__(data, "choice")
 
 class PrefixNode(BaseNode):
 
-    def __init__(self, data, asttype):
-        super(PrefixNode, self).__init__(data, asttype)
+    def __init__(self, data):
+        super(PrefixNode, self).__init__(data, "prefix")
 
 class DefNode(BaseNode):
 
-    def __init__(self, data, asttype):
-        super(DefNode, self).__init__(data, asttype)
+    def __init__(self, data):
+        super(DefNode, self).__init__(data, "definition")
 
 class ActivityNode(BaseNode):
 
-    def __init__(self, data, asttype):
+    def __init__(self, data):
         self.action = ""
         self.rate = ""
-        super(ActivityNode, self).__init__(data, asttype)
+        super(ActivityNode, self).__init__(data, "activity")
 
 class ProcdefNode(BaseNode):
 
-    def __init__(self, data, asttype):
+    def __init__(self, data):
         self.aggregation = None
         self.aggr_num = 0
-        super(ProcdefNode, self).__init__(data, asttype)
+        super(ProcdefNode, self).__init__(data, "procdef")
 
 class CoopNode(BaseNode):
 
-    def __init__(self, data, asttype):
+    def __init__(self, data):
         self.actionset = None
         self.cooptype = None
-        super(CoopNode, self).__init__(data, asttype)
+        super(CoopNode, self).__init__(data, "coop")
 
 class SyncsetNode(BaseNode):
 
-    def __init__(self, data, asttype):
+    def __init__(self, data):
         self.actionset = None
-        super(SyncsetNode, self).__init__(data, asttype)
+        super(SyncsetNode, self).__init__(data, "syncset")
 
